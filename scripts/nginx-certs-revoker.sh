ea7fbf9b411e0cc7071e221f8f9a97d880c5ee30
desimaniac <desimaniac@users.noreply.github.com>
2017-08-12 04:44:21 -0500

    Update nginx-certs-revoker.sh

--- scripts/nginx-certs-revoker.sh
+++ scripts/nginx-certs-revoker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-domain=<insertdomain.com>
+domain=
+
+cd /opt/nginx-proxy
 
 for i in *.${domain}.chain.pem;
   do
     base=${i%%??????????}
     sudo certbot revoke --cert-path ${base}/fullchain.pem --key-path ${base}/key.pem
-    #echo ${base}
 done
